a69f2b33a5c98b7c37c0fd1a5b8a5f6d5ca50613
xaviaguilarb <xaviaguilar071@gmail.com>
2026-06-03 12:52:03 +0200

    Create codi.ino

---
+++ src/codi.ino
@@ -0,0 +1,34 @@
+﻿// Definimos el pin del LED
+const int LED_PIN = 17;  // Ahora usamos GPIO17
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);  // LED apagado inicialmente
+  
+  Serial.begin(115200);
+  
+  delay(1000);
+  Serial.println("Sistema listo. Escribe ON u OFF para controlar el LED");
+}
+
+void loop() {
+  if (Serial.available()) {
+    String linea = Serial.readStringUntil('\n');
+    linea.trim();
+    
+    Serial.print("Recibido: ");
+    Serial.println(linea);
+    
+    if (linea.equalsIgnoreCase("ON")) {
+      digitalWrite(LED_PIN, HIGH);
+      Serial.println("LED ENCENDIDO");
+    }
+    else if (linea.equalsIgnoreCase("OFF")) {
+      digitalWrite(LED_PIN, LOW);
+      Serial.println("LED APAGADO");
+    }
+    else if (linea.length() > 0) {
+      Serial.println("Comando desconocido");
+    }
+  }
+}
